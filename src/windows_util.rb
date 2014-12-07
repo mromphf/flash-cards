6d07e3ce8534bd60d410f11814cbcf7ba11cf28b
mromphf <markromphf@gmail.com>
2014-12-07 13:30:13 -0600

    Slight changes to the window layout.

--- src/windows_util.rb
+++ src/windows_util.rb
@@ -8,18 +8,10 @@ class WindowsUtil
   include Curses
   include GlobalConfig
 
-  def self.build_answer_window
-    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 25, (cols - WINDOW_LENGTH) / 2)
-    win.box(?|, ?-)
-    win.setpos(2, 3)
-    win.addstr("ANSWER: ")
-    win.getstr.to_i
-  end
-
   def self.build_results_window(results)
     clear
     refresh
-    win = Window.new(WINDOW_HEIGHT + 1, WINDOW_LENGTH, 11, (cols - WINDOW_LENGTH) / 2)
+    win = Window.new(WINDOW_HEIGHT + 1, WINDOW_LENGTH, 8, (cols - WINDOW_LENGTH) / 2)
 
     win.box(?|, ?-)
     win.setpos(2, 3)
@@ -30,7 +22,7 @@ class WindowsUtil
   end
 
   def self.build_score_window(feedback = "")
-    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 11, (cols - WINDOW_LENGTH) / 2)
+    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 7, (cols - WINDOW_LENGTH) / 2)
     win.box(?|, ?-)
     win.setpos(2, 3)
     win.addstr("Last Answer: #{feedback}")
@@ -38,10 +30,19 @@ class WindowsUtil
   end
 
   def self.build_question_window(question)
-    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 18, (cols - WINDOW_LENGTH) / 2)
+    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 14, (cols - WINDOW_LENGTH) / 2)
     win.box(?|, ?-)
     win.setpos(2, 3)
     win.addstr("#{question.to_s}")
     win.refresh
   end
+
+  def self.build_answer_window
+    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 21, (cols - WINDOW_LENGTH) / 2)
+    win.box(?|, ?-)
+    win.setpos(2, 3)
+    win.addstr("ANSWER: ")
+    win.getstr.to_i
+  end
+
 end
