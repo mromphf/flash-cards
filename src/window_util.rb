f03af8ac84f85366f0fc5d982214746b3c991a10
Mark Romphf <markromphf@gmail.com>
2014-12-12 22:07:11 -0600

    Changed the window util to render top window ten percent from the top of the screen.

--- src/window_util.rb
+++ src/window_util.rb
@@ -9,7 +9,8 @@ class WindowUtil
   WINDOW_LENGTH = 35
 
   def self.build_score_window(score = "")
-    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 7, self.screen_center)
+    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 
+                     self.top_ten_percent, self.screen_center)
     win.box(?|, ?-)
     win.setpos(2, 3)
     win.addstr("Score: #{score}")
@@ -17,7 +18,8 @@ class WindowUtil
   end
 
   def self.build_question_window(question)
-    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 14, self.screen_center)
+    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 
+                     self.top_ten_percent + 7, self.screen_center)
     win.box(?|, ?-)
     win.setpos(2, 3)
     win.addstr("#{question.to_s}")
@@ -25,7 +27,8 @@ class WindowUtil
   end
 
   def self.build_answer_window
-    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 21, self.screen_center)
+    win = Window.new(WINDOW_HEIGHT, WINDOW_LENGTH, 
+                     self.top_ten_percent + 14, self.screen_center)
     win.box(?|, ?-)
     win.setpos(2, 3)
     win.addstr("ANSWER: ")
@@ -35,7 +38,7 @@ class WindowUtil
   def self.build_results_window(reporter)
     clear
     refresh
-    win = Window.new(18, 45, 8, (cols - 45) / 2)
+    win = Window.new(18, 45, self.top_ten_percent, (cols - 45) / 2)
     win.box(?|, ?-)
     win.setpos(2, 3)
     win.addstr("#{reporter.header}")
@@ -53,5 +56,9 @@ class WindowUtil
   private
     def self.screen_center
       (cols - WINDOW_LENGTH) / 2
+    end
+
+    def self.top_ten_percent
+      (lines.to_f * 0.1).to_i
     end
 end
