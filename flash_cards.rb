30c1620201121af74fc23e3167258b997c860619
Mark Romphf <markromphf@gmail.com>
2014-12-12 21:52:18 -0600

    Wrapped the source of the execute script in a class.

--- flash_cards.rb
+++ flash_cards.rb
@@ -4,36 +4,50 @@ require_relative 'src/arg_utility'
 require_relative 'src/window_util'
 require_relative 'src/quiz_factory'
 require_relative 'src/reporter'
-include ArgUtility
 include Curses
 
-def take_quiz
-  quiz = QuizFactory.generate_quiz(ARGV[0], ARGV[1])
-  reporter = Reporter.new
-  WindowUtil.build_score_window
-  quiz.size.times do
-    question = quiz.get_random_question
-    WindowUtil.build_question_window(question)
-    answer = WindowUtil.build_answer_window
-    quiz = quiz.answer_question(question, answer)
-    reporter = reporter.add_entry(question, answer)
-    WindowUtil.build_score_window(quiz.display_score)
+class FlashCards
+  include ArgUtility
+
+  def main
+    if valid_args?(ARGV)
+      initialize_interface
+    else
+      puts usage_info
+    end
   end
-  WindowUtil.build_results_window(reporter)
+
+  private
+    def take_quiz
+      quiz = QuizFactory.generate_quiz(ARGV[0], ARGV[1])
+      reporter = Reporter.new
+      WindowUtil.build_score_window
+      quiz.size.times do
+        question = quiz.get_random_question
+        WindowUtil.build_question_window(question)
+        answer = WindowUtil.build_answer_window
+        quiz = quiz.answer_question(question, answer)
+        reporter = reporter.add_entry(question, answer)
+        WindowUtil.build_score_window(quiz.display_score)
+      end
+      WindowUtil.build_results_window(reporter)
+    end
+
+    def initialize_interface
+      init_screen
+      begin
+        crmode
+        take_quiz
+      ensure
+        close_screen
+      end
+    end
 end
 
-def initialize_interface
-  init_screen
+if $0 == __FILE__
   begin
-    crmode
-    take_quiz
-  ensure
-    close_screen
+    FlashCards.new.main
+  rescue Interrupt
+    exit(1)
   end
-end
-
-if valid_args?(ARGV)
-  initialize_interface
-else
-  puts usage_info
 end
